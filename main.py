c1406970ab6114fe0e05c36e8fdd4169c6111e7d
Ibragim <tamaevmikail8@gmail.com>
2024-03-02 17:28:33 +0300

    a

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 print("hello")
 """
-git init
-git status
-git add .
-git commit -m "comment"
-
+git init - инициализация проекта
+git status - проверка статуса гита
+git add . - промежуточное сохранение
+git commit -m "comment" - сохранение
+git branch - проверка веток
+git branch name_branch - создать новую ветку
+git checkout name_branch - перейти на вептку
+git mergre - объединить две ветки
+git push - добавить на гитхаб
 """
